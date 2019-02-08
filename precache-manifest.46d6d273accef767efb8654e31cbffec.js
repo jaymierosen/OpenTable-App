24200966349dd281af0a5fba7fd288975db1be80
jaymierosen <rosen.jaymie@gmail.com>
2019-02-07 20:25:48 -0500

    Updates

---
+++ precache-manifest.46d6d273accef767efb8654e31cbffec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e54656acf445ee66b34a",
+    "url": "/opentable-app/static/js/runtime~main.e54656ac.js"
+  },
+  {
+    "revision": "4f775aaf4bf2cb27c233",
+    "url": "/opentable-app/static/js/main.4f775aaf.chunk.js"
+  },
+  {
+    "revision": "2f16ecc4231c7f49989e",
+    "url": "/opentable-app/static/js/1.2f16ecc4.chunk.js"
+  },
+  {
+    "revision": "4f775aaf4bf2cb27c233",
+    "url": "/opentable-app/static/css/main.decc95ae.chunk.css"
+  },
+  {
+    "revision": "ae1a88338294294c75133f7fae1e051a",
+    "url": "/opentable-app/index.html"
+  }
+];
